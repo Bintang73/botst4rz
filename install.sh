f5fb09885d8230c0c586a6dd40636b8316a3ec79
Bintang73 <42708989+Bintang73@users.noreply.github.com>
2020-10-31 13:58:35 +0700

    Fixed bug

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@ apt update
 apt upgrade
 apt install
 pkg update && pkg upgrade
-pkg install wget -y
-pkg install ffmpeg -y
-pkg install nodejs -y
+pkg install wget
+pkg install ffmpeg
+pkg install nodejs
 npm i -g cwebp
 npm i -g ytdl 
 npm i
