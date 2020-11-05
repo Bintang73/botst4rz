c9894fa0d83198777221bcbae75e9b5553b4c4f4
farjihan <55767704+farjihan@users.noreply.github.com>
2020-11-05 15:22:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const menu = require("./lib/menu.js");
 const donate = require("./lib/donate.js");
 const info = require("./lib/info.js");
 //
-const BotName = 'ST4RZ BOT 🤖'; // Nama Bot Whatsapp
+const BotName = 'HANZ BOTʕ·ᴥ·ʔ'; // Nama Bot Whatsapp
 const instagramlu = 'https://instagram.com/bintang_nur_pradana'; // Nama Instagramlu cok
 const whatsapplu = '0819-4698-3575'; // Nomor whatsapplu cok
 const kapanbotaktif = '24 Jam'; // Kapan bot lu aktif
